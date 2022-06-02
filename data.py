fa703bcd00642255387f992b77ab8d5ab6ba8b44
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-06-02 13:44:49 -0400

    updates

--- data.py
+++ data.py
@@ -1,9 +1,21 @@
 #Maria Williams
-#6/1/22: loading and manipulating dcm files
+#6/2/22: loading and manipulating dcm files
 
 import pydicom as dicom
 import matplotlib.pylab as plt
 
-img = dicom.dcmread('testimg.dcm')
-plt.imshow(img.pixel_array, cmap=plt.cm.bone)
-plt.savefig('testimg')
+#load and convert dcm to jpg
+from pydicom import dcmread
+from pydicom.pixel_data_handlers import convert_color_space
+
+image_path = 'BrainAgingComputerVision/testimg1.dcm'
+img = dicom.dcmread(image_path)
+new_image = img.pixel_array.astype(float)
+
+#convert to jpg
+scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255.0
+scaled_image = np.uint8(scaled_image)
+final_image = Image.fromarray(scaled_image)
+final_image.show()
+final_image.save('testimgdone.jpg')
+print(final_image.size)
