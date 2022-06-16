ce52735044163b5a6e97db977821fd72e8f3cc30
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-06-16 15:10:14 -0400

    predictions

---
+++ scripts/Rpredicting.py
@@ -0,0 +1,22 @@
+#Maria Williams
+#Last Modified: 6/16/22
+#Regression prediction script
+
+import torch
+import numpy as np
+
+# Function to get predictions
+def get_results(model,dataloader,device):
+    model = model.to(device) # Send model to GPU if available
+    with torch.no_grad():
+        model.eval()
+        # Get a batch of validation images
+        images, labels = iter(dataloader).next()
+        images, labels = images.to(device), labels.to(device)
+        # Get predictions
+        preds = model(images)
+        preds = np.squeeze(preds.cpu().numpy())
+        preds = np.around(preds, decimals=1)
+        labels = labels.cpu().numpy()
+    
+    return preds, labels
